fbca85b28f5c716b281af50944ecbf9e155a43bb
Jhester11 <102759486+Jhester11@users.noreply.github.com>
2023-06-12 11:10:32 +0800

    Fundamentals in react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Library
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Css path
+import "./index.css";
+const firstBook = {
+    title: "Oh, the Places You'll Go!  ",
+    author: "Dr. Seuss",
+    img: './images/book-1.jpg',
+};
+const secondBook = {
+    title: "Happy Place",
+    author: "Emily Henry",
+    img: './images/book-2.jpg',
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// For component
+const BookList = () => {
+    return (
+        <section className="booklist">
+            <Book title={firstBook.title} author={firstBook.author} img={firstBook.img} />
+            <Book title={secondBook.title} author={secondBook.author} img={secondBook.img}/>
+        </section>
+    );
+};
+
+const Book = (props) => {
+    console.log(props)
+    return (
+        <article className="book">
+            <img src={props.img} alt={props.title}></img>;
+            <h2>{props.title}</h2>;
+            <h4>{props.author}</h4>
+            {console.log(props)};
+        </article>
+    );
+};
+
+// For Rendering
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
